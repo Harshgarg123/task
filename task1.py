a911eb7777825d817eac461d8288f75c02bdfb82
Harshgarg123 <142335665+Harshgarg123@users.noreply.github.com>
2025-08-02 21:16:17 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,12 @@
+a=int(input("Enter first number"))
+b=int(input("Enter second number"))
+Addition=a+b
+Subtraction=a-b
+Multliplication =a*b
+Division=a/b
+
+print("Addition:",Addition)
+print("Subtraction:",Subtraction)
+print("Multliplication:",Multliplication)
+print("Division:",Division)
+
---
+++ task2.py
@@ -0,0 +1,3 @@
+a=input("Enter first name:")
+b=input("Enter second name:")
+print(f"Hello, {a} {b}! Welcome to the Python program")
